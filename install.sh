5f015793aaa824648d1b8db9ed9249dc82b637df
zotochev <mikhail@digitalcube.rs>
2023-07-13 11:04:45 +0200

    Extended installation script to support non existing installation directory

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+# export IFS=":"
+# sentence="$PATH"
+INSTALLATION_DIR="$HOME/.local/bin"
+
+if [[ $PATH == *"$INSTALLATION_DIR"* ]]; then
+  # echo "EXISTS IN PATH!"
+  :
+else
+  for word in $sentence; do
+    if [[ $word == *"local"* ]]; then
+      INSTALLATION_DIR="$word"
+      break
+    fi
+  done
+fi
+
+if [ ! -d "$INSTALLATION_DIR" ]; then
+  # echo "NO such directory"
+  mkdir -p "$INSTALLATION_DIR"
+fi
+
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cp "$SCRIPT_DIR/git-puller" "$HOME/.local/bin"
+cp "$SCRIPT_DIR/git-puller" "$INSTALLATION_DIR"
+
